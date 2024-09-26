27f4e045a050ba0aad7fae68d01c1c8f17fc5302
kayke <kayke.py@gmail.com>
2024-09-25 21:22:11 -0300

    feat: Funcionalidade de criação de um post criado

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
   layout 'dashboard'
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    return redirect_to posts_path, status: :created if @post.save!
+    render :new, status: :unprocessable_entity
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:description, :number_of_likes, :image)
   end
 end
