5af2bb3f275091a2bc1eb9ec40450ca0158721b3
kayke <kayke.py@gmail.com>
2024-10-01 20:20:13 -0300

    feat: Rota criada para receber solicitações get para acrescentar um like ao post

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "site#index"
   get '/site', to: 'site#index'
-  resources :posts
+  resources :posts do 
+    get "/like", to: 'posts#handle_likes'
+  end
 end
