d3bfa67b3d87bf7a40edad4879ff8a3302844cf1
kayke <kayke.py@gmail.com>
2024-09-30 21:12:12 -0300

    feat: Novo formato de resposta na action create, turbo_stream utilizado para atualizar um frame unico sem alterar os outros. Before actio set_post adicionado para action destroy. Action destroy criada para destruir uma instancia de um post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   layout 'dashboard'
+  before_action :set_post, only: [:destroy]
   def index
-    @posts = Post.all
+    @posts = Post.ordered
   end
 
   def new
@@ -11,12 +12,26 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    return redirect_to posts_path, status: :created if @post.save!
-    render :new, status: :unprocessable_entity
+    if @post.save!
+      respond_to do |format|
+        format.html {redirect_to(posts_path, status: :created, notice: 'Post was successfuly created')}
+        format.turbo_stream
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
+  def destroy 
+    @post.destroy
+    redirect_to posts_path, status: :see_other
+  end
 
   private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:description, :number_of_likes, :image)
   end
