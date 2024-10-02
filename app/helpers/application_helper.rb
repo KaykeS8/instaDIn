4ba034418d72f97aeae68646ca6ad6b62bc1e297
kayke <kayke.py@gmail.com>
2024-10-02 20:21:00 -0300

    feat: Action para auxiliar na renderização do ultimo user que deu um like

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
     def already_liked?(post)
         Like.where(user_id: current_user.id, post: post.id).exists?
     end
+
+    def last_user_of_like(post)
+        like = Like.where(post_id: post.id).last
+        if like
+            name_of_the_last_liked = like.user.name
+            "Liked by #{name_of_the_last_liked} and others"
+        else
+            "be the first to like"
+        end
+    end
 end
