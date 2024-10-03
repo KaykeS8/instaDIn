ce17f813b9d0519dd2585d9a6db6874c3090985a
kayke <kayke.py@gmail.com>
2024-10-03 19:46:23 -0300

    Associações com o model comments adicionada

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   
   validates :email, uniqueness: true
   has_many :likes, dependent: :destroy
-  has_many :posts
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
