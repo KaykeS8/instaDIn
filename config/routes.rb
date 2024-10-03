c17cefa71f29bc420c382fa1af30982c37ffbb0b
kayke <kayke.py@gmail.com>
2024-10-02 21:06:57 -0300

    Feat: Rota para index adicionada

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root "site#index"
   get '/site', to: 'site#index'
   resources :posts do 
-    resources :likes, only: [:create, :destroy]
+    resources :likes, only: [:index, :create, :destroy]
   end
 end
