346fda330a7b9b7cb44fde46d3f36ed7834de13c
kayke <kayke.py@gmail.com>
2024-10-09 20:15:04 -0300

    fix: Order adicionado na action index para buscar os comentarios com base no ID e respond_to adicionado a action create para responder em 2 formatos diferentes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     before_action :find_post, only: [:index, :create]
 
     def index
-        @comments = Comment.where(post_id: params[:post_id])
+        @comments = Comment.where(post_id: params[:post_id]).order(id: :desc)
         @comment = Comment.new
     end
     
@@ -11,7 +11,10 @@ class CommentsController < ApplicationController
         @comment = @post.comments.create(comment_params)
         @comment.user_id = current_user.id
         if @comment.save
-            redirect_to post_comments_path(@post), status: :created
+            respond_to do |format|
+                format.html { redirect_to(post_comments_path(@post), status: :created)}
+                format.turbo_stream 
+            end
         else
             render :index, status: :unprocessable_entity
         end
