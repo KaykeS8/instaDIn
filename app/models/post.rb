d8e7d9aa4b2434f4f55c3bd42b4164eb6651b0e2
kayke <kayke.py@gmail.com>
2024-10-13 17:47:57 -0300

    Feat: Faz Validação da imagem do post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_one_attached :image
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  validates :image, presence: true
 
   scope :ordered, -> {order(id: :desc)}
 end
