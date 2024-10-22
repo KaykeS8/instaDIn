d4fa7fb7349669c30e32eb5719bc935066160428
kayke <kayke.py@gmail.com>
2024-10-22 20:00:57 -0300

    Métodos para redirecionar o usuario caso não tenha um profile criado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :check_profile, expect: [:new, :create, :edit, :update, :destroy], if: :devise_controller?
 
     protected
     def configure_permitted_parameters
@@ -8,5 +9,15 @@ class ApplicationController < ActionController::Base
     
     def after_sign_in_path_for(resource)
         posts_path
+    end
+
+    def after_sign_up_path_for(resource)
+        new_profile_path
+    end
+
+    def check_profile
+        if current_user && current_user.profile.nil?
+            redirect_to new_profile_path, alert: 'Please create your profile'
+        end
     end
 end
