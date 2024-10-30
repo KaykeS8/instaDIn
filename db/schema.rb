91d6303b8ae4b2e99912bb405e1b99ba0f298130
kayke <kayke.py@gmail.com>
2024-10-30 20:11:33 -0300

    Tabela friends adicionado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_13_212018) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_24_230900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_13_212018) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_friends_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_friends_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_friends_on_follower_id"
   end
 
   create_table "likes", force: :cascade do |t|
