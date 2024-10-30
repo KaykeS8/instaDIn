7b62113e3d43492528ce41732f1cdb87b8cd7901
kayke <kayke.py@gmail.com>
2024-10-30 20:11:55 -0300

    Feat
    git commit -m 'feat
    qqquit
    quit
    '

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :followers, class_name: "Friend" , foreign_key: "follower_id", dependent: :destroy
 end
