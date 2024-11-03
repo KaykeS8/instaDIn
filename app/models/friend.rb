28c867f7d01273d37306ceff4a7137bf5e1c5699
kayke <kayke.py@gmail.com>
2024-11-03 13:12:25 -0300

    Validação dos campos

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,8 @@
 class Friend < ApplicationRecord
     belongs_to :followed, class_name: 'User'
     belongs_to :follower, class_name: 'User'
+
+    validates :follower_id, presence: true
+    validates :followed_id, presence: true
+    validates :followed_id, uniqueness: {scope: :follower_id, message: "Ja esta na lista de amigos"}
 end
