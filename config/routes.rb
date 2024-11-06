d4d38121066691f82951b6375abdff96d6674835
kayke <kayke.py@gmail.com>
2024-11-05 21:18:35 -0300

    Rotas adicionadas para followers and followings

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root "site#index"
   get '/site', to: 'site#index'
+
   resources :posts do 
     resources :likes, only: [:index, :create, :destroy]
     resources :comments, only: [:index, :create, :destroy, :edit]
   end
+
   resources :profiles, param: :user_name
-  resources :friends, only: [:new, :create, :destroy]
+  get '/followers/:user_name', to: 'profiles#followers'
+  get '/followings/:user_name', to: 'profiles#followings'
+  
+  resources :friends, only: [:new, :create, :destroy, :index]
   post '/friends/:id', to: 'friends#create'
 end
