5772c52513291585033674522f9fee370d317ebd
kayke <kayke.py@gmail.com>
2024-11-21 21:23:58 -0300

    Nome do método alterada para :chat_exists? e metodo seek_chat refatorado

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -13,7 +13,7 @@ class ChatsController < ApplicationController
   end
 
   def create
-    unless check_if_chat_exist?
+    unless chat_exist?
       @chat = Chat.create!(sender_user_id: current_user.id, recipient_user_id: fetch_recipient.id)
         redirect_to(@chat)
     else
@@ -26,13 +26,13 @@ class ChatsController < ApplicationController
     User.find(params[:recipient_id])
   end
 
-  def check_if_chat_exist?
+  def chat_exist?
     Chat.exists?(sender_user_id: current_user.id, recipient_user_id: fetch_recipient.id) || Chat.exists?(sender_user_id: fetch_recipient.id, recipient_user_id: current_user.id)
   end
 
   def seek_chat
-    @chat = Chat.find_by!(sender_user_id: current_user.id, recipient_user_id: fetch_recipient.id)
+    @chat = Chat.between_users(current_user, fetch_recipient).first
   end
 end
 
-# PRECISO PENSAR EM UMA MANEIRA DE SINCRONIZAR OS CHATS DOS USUARIOS
+#PRECISO PENSAR EM COMO CARREGAR AS MENSAGENS DE UM CHAT ESPECIFICO
